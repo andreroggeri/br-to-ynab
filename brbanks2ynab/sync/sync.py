71e82e289162f39460c05776fe9c57ab379a041e
André Roggeri Campos <a.roggeri.c@gmail.com>
2021-11-21 19:59:17 -0300

    feat: Add dry parameter for testing

--- brbanks2ynab/sync/sync.py
+++ brbanks2ynab/sync/sync.py
@@ -12,7 +12,7 @@ from brbanks2ynab.ynab.ynab_transaction_importer import YNABTransactionImporter
 logger = logging.getLogger('brbanks2ynab')
 
 
-def sync(config_file_path: Path):
+def sync(config_file_path: Path, dry: bool):
     if not config_file_path.exists():
         raise Exception(f'Arquivo de configuração "{config_file_path}" não encontrado')
 
@@ -31,11 +31,11 @@ def sync(config_file_path: Path):
         for importer in importers:
             ynab_importer.get_transactions_from(importer)
 
-    response = ynab_importer.save()
-    print(ynab_importer.transactions)
-    print(response)
-
-    print(f'{len(response["importers"]["transaction_ids"])} new transactions imported')
+    if dry:
+        print(f'{len(ynab_importer.transactions)} would be imported into YNAB')
+    else:
+        response = ynab_importer.save()
+        print(f'{len(response["importers"]["transaction_ids"])} transactions imported')
 
 
 if __name__ == '__main__':
